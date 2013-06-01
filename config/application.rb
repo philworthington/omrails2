a26bb4940585295afae7c766e2ce45d2614a4dd2
Phil Worthington <pworthington27@gmail.com>
2013-06-01 19:52:55 -0400

    Add User Model With Devise

--- config/application.rb
+++ config/application.rb
@@ -56,6 +56,9 @@ module Omrails
     # Enable the asset pipeline
     config.assets.enabled = true
 
+    # Required for Devise on Heroku
+    config.assets.initialize_on_precompile = false
+
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
   end
